89666966ac10c1e61c826ee0d9af1c4f407547ac
asifgenius <asif15-11288@diu.edu.bd>
2024-05-18 16:55:25 +0600

    Add constants.js

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const HEAP_FILE_PATH = '/tmp/heap.pb.gz';
+export const WALL_FILE_PATH = '/tmp/wall.pb.gz';
+export const HEAP_WEB_PATH = '/debug/pprof/heap';
+export const HEAP_STOP_PATH = '/debug/pprof/heap/stop';
+export const WALL_WEB_PATH = '/debug/pprof/wall';
+export const INTERVAL_BYTES = 512 * 1024;
+export const STACK_DEPTH = 64;
